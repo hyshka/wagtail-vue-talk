ae16e0e819351f3d7ed5dbbaf2480234e50be1a7
Bryan Hyshka <bryan@hyshka.com>
2018-11-14 12:40:40 -0700

    Banner style

--- vue/src/views/HomePage.vue
+++ vue/src/views/HomePage.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="page">
     <!-- banner -->
-    <div class="mw8 center tc ph4 pv5">
-      <h1 class="fw7 f1 lh-title mt0 mb3">
-        {{ this.page.title }}
-      </h1>
-      <h2 class="fw4 f2 lh-copy mt0 mb3">
-        {{ this.page.banner_subtitle }}
-      </h2>
+    <div class="relative">
+      <div class="absolute absolute--fill flex items-center">
+        <div class="mw7 center tc ph4 pv5 white">
+          <h1 class="fw7 f1 lh-title mv0">
+            {{ this.page.title }}
+          </h1>
+          <p class="fw4 f3 lh-copy mt3 mb0">
+            {{ this.page.banner_subtitle }}
+          </p>
+        </div>
+      </div>
       <wagtail-image
         :id="this.page.banner_image.id"
         ></wagtail-image>
